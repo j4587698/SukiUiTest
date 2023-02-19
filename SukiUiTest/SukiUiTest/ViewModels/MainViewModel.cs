349af290cd554fe89fe33a7cc34c74a5b5994104
jx <jx@jvxiang.com>
2023-02-19 20:15:26 +0800

    RiderAvalonia Test

--- SukiUiTest/SukiUiTest/ViewModels/MainViewModel.cs
+++ SukiUiTest/SukiUiTest/ViewModels/MainViewModel.cs
@@ -1,7 +1,14 @@
-﻿namespace SukiUiTest.ViewModels
+﻿using LiteDB;
+
+namespace SukiUiTest.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
         public string Greeting => "Welcome to Avalonia!";
+        
+        public MainViewModel()
+        {
+            var db = new LiteDatabase("MyData.db");
+        }
     }
 }
